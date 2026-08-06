d29ce2ac1257187dfa57d368a743872865e0fc94
rokhill <rokhill1@gmail.com>
2026-08-06 02:35:56 -0500

    swap: clean amber best price routing label

--- components/swap-form.tsx
+++ components/swap-form.tsx
@@ -351,7 +351,7 @@ export default function SwapForm() {
             </li>
             <li className="">
               <span>Order routing</span>
-              <span className="forge-breathe" style={{ color: "var(--ae-ember-bright)", fontWeight: 600 }}>⚡ Best price routing</span>
+              <span style={{ color: "var(--ae-aurum)" }}>⚡ Best price routing</span>
             </li>
             <li className="">
               <span>Max slippage</span>
