21601726df970a0fedd26c109287d467440f015a
rokhill <rokhill1@gmail.com>
2026-08-06 02:01:11 -0500

    swap: show best price routing label instead of Uniswap API

--- components/swap-form.tsx
+++ components/swap-form.tsx
@@ -351,7 +351,7 @@ export default function SwapForm() {
             </li>
             <li className="">
               <span>Order routing</span>
-              <span>Uniswap API</span>
+              <span style={{ color: "var(--ae-aurum)" }}>⚡ Best price routing</span>
             </li>
             <li className="">
               <span>Max slippage</span>
